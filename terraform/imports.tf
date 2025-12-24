a815fee97f7fa3d9ca6ea718cb61db72220ae155
Abby263 <abhayraj.mkp7@gmail.com>
2025-12-24 09:52:57 +0530

    Refactor Terraform import instructions for Azure resources
    - Streamlined the import blocks for existing Azure resources in the imports.tf file, making them active by default for easier usage.
    - Clarified comments to guide users on the import process and the importance of commenting out blocks after successful imports.

--- terraform/imports.tf
+++ terraform/imports.tf
@@ -1,31 +1,26 @@
 # Import existing Azure resources into Terraform state
-# These resources were created but the state was lost/reset
-# 
-# IMPORTANT: Only uncomment these import blocks if you get errors about
-# resources already existing. Comment them back out after successful import.
-#
-# To use: uncomment the relevant import block, run terraform plan/apply,
-# then comment it back out after the resource is imported.
+# These resources exist in Azure but are not in Terraform state
+# After successful import, comment these blocks back out
 
-# # Import existing Resource Group (uncomment if resource already exists)
-# import {
-#   to = azurerm_resource_group.main
-#   id = "/subscriptions/${var.subscription_id}/resourceGroups/${local.resource_prefix}-rg"
-# }
+# Import existing Resource Group
+import {
+  to = azurerm_resource_group.main
+  id = "/subscriptions/${var.subscription_id}/resourceGroups/${local.resource_prefix}-rg"
+}
 
-# # Import existing Container Registry (uncomment if resource already exists)
-# import {
-#   to = module.container_registry.azurerm_container_registry.this
-#   id = "/subscriptions/${var.subscription_id}/resourceGroups/${local.resource_prefix}-rg/providers/Microsoft.ContainerRegistry/registries/${replace("${local.resource_prefix}acr", "-", "")}"
-# }
+# Import existing Container Registry
+import {
+  to = module.container_registry.azurerm_container_registry.this
+  id = "/subscriptions/${var.subscription_id}/resourceGroups/${local.resource_prefix}-rg/providers/Microsoft.ContainerRegistry/registries/${replace("${local.resource_prefix}acr", "-", "")}"
+}
 
-# # Import existing Log Analytics Workspace (uncomment if resource already exists)
-# import {
-#   to = module.container_environment.azurerm_log_analytics_workspace.this
-#   id = "/subscriptions/${var.subscription_id}/resourceGroups/${local.resource_prefix}-rg/providers/Microsoft.OperationalInsights/workspaces/${local.resource_prefix}-env-logs"
-# }
+# Import existing Log Analytics Workspace
+import {
+  to = module.container_environment.azurerm_log_analytics_workspace.this
+  id = "/subscriptions/${var.subscription_id}/resourceGroups/${local.resource_prefix}-rg/providers/Microsoft.OperationalInsights/workspaces/${local.resource_prefix}-env-logs"
+}
 
-# # Import existing Container App Environment (uncomment if resource already exists)
+# Import existing Container App Environment (comment out if it doesn't exist yet)
 # import {
 #   to = module.container_environment.azurerm_container_app_environment.this
 #   id = "/subscriptions/${var.subscription_id}/resourceGroups/${local.resource_prefix}-rg/providers/Microsoft.App/managedEnvironments/${local.resource_prefix}-env"
